a5c1407c782342fa64fffb46331dfd20f60320f1
chakra4200 <chakrabortyipshita12@gmail.com>
2026-06-29 20:09:40 +0530

    Change database credentials and connection details
    Updated database connection details for security and clarity.

--- Expenses/mydb_Connection.py
+++ Expenses/mydb_Connection.py
@@ -4,8 +4,8 @@ def create_connection() -> MySQLdb.Connection:
     return MySQLdb.connect(
         host = "localhost",
         user ="root",
-        password ="Mysql@123",
-        database = "my_first_db"
+        password ="Your_password",
+        database = "Your_Database"
         )
 
 def create_tables():
